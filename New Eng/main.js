9953abc3a72f2ebe5f9956cea579750d8106e0aa
aacczury <yl931905@gmail.com>
2014-08-18 13:39:12 +0800

    add files

---
+++ New Eng/main.js
@@ -0,0 +1,69 @@
+
+<!--
+var teacher_image = new Array(14);
+teacher_image[0] = "CTC";
+teacher_image[1] = "FTF";
+teacher_image[2] = "HHS";
+teacher_image[3] = "HWY";
+teacher_image[4] = "LTM";
+teacher_image[5] = "SSC";
+teacher_image[6] = "WJJ";
+teacher_image[7] = "WJR";
+teacher_image[8] = "YKC";
+teacher_image[9] = "YTM";
+teacher_image[10] = "CCF";
+teacher_image[11] = "TPJ";
+teacher_image[12] = "HLJ";
+teacher_image[13] = "CPS";
+
+var teacher_profile = new Array(14);
+teacher_profile[0] = "Professor Tsung-Chain Chang\n(Biotechnology, Cell Biology)\n\n\n";
+teacher_profile[1] = "Assistant Professor Tzu-Fun Fu\n(Enzymology, Folate-mediated one-carbon metabolism)\n\n";
+teacher_profile[2] = "Assistant Professor Huei-Sheng Huang\n(Molecular Toxicology, Free Radical in Medicine, Optical Biosensors)\n";
+teacher_profile[3] = "Professor Wen-Ya Huang\n(Molecular Pathology, Clinical Hematology)\n";
+teacher_profile[4] = "Associate Professor Tsun-Mei Lin\n(Clinical Hematology, Immunohematology, Vascular Biology, Infection and Inflammation)";
+teacher_profile[5] = "Professor Shu-Chu Shiesh\n(Clinical Chemistry, Metabonomics, Biosensor)\n\n";
+teacher_profile[6] = "Professor Jiunn-Jong Wu\n(Microbiology, Clinical Microbiology, Molecular Biology)\n\n";
+teacher_profile[7] = "Professor Jen-Ren Wang\n(Clinical Microbiology, Clinical Virology, Microbiology)\n\n";
+teacher_profile[8] = "Associate Professor Kung-Chia Young\n(Virology, Molecular Biology, Clinical Microscopy)\n";
+teacher_profile[9] = "Professor Trai-Ming Yeh\n(Microbiology, Immunology, Clinical Serology and Immunology)\n";
+teacher_profile[10] = "Assistant Professor Chuan-Fa Chang\n(Glycoscience, Glycobiology, High-Troughput Screening, Sugar Array)\n";
+teacher_profile[11] = "Assistant Professor Pei-Jane Tsai\n(Clinical Microbiology Bacterial, Pathogenesis, Infectious, Disease, Animal Disease Model)\n";
+teacher_profile[12] = "Assistant Professor Li-Jin Hsu\n(Immunology, Cancer Biology, Molecular Biolog, Signal Transduction)\n"; 
+teacher_profile[13] = "Assistant Professor Pai-Sheng Chen\n(Tumor biology, microRNA, Molecular cancer therapeutics)\n"; 
+
+var research_image = new Array(8);
+research_image[0] = "cover_01";
+research_image[1] = "cover_02";
+research_image[2] = "cover_03";
+research_image[3] = "cover_04";
+research_image[4] = "cover_05";
+research_image[5] = "cover_06";
+research_image[6] = "cover_07";
+research_image[7] = "cover_08";
+research_image[8] = "cover_09";
+
+var n_RT = Math.round(Math.random()*13);
+var n_RR = Math.round(Math.random()*8);
+
+function init_inside()
+{
+  document.TResearch01.src="../2096/"+research_image[n_RR]+"_01.jpg";
+}
+
+function init_inside_2()
+{
+  document.TResearch01.src="../2096/"+research_image[n_RR]+"_01.jpg";
+}
+
+function winOpen(obj_link, obj_title, obj_width, obj_height, scroll_bar){
+   window.open(obj_link,obj_title,"fullscreen=0,location=0,directories=0,status=0,scrollbars="+scroll_bar+",resizable=0,toolbar=no,menubar=no,left=100,top=100,width="+obj_width+",height="+obj_height);
+}
+
+function show_hide(id){
+  if (id.style.display=="none") id.style.display="";
+  else{
+    id.style.display="none";
+  }
+}
+//-->
